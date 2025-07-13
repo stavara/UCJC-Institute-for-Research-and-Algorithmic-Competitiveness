d21d0a8e42606dcd6203f2107fabd6eb60979a0f
stavara <42417471+stavara@users.noreply.github.com>
2025-07-13 23:14:41 +0200

    Menu mobile

--- js/script.js
+++ js/script.js
@@ -64,3 +64,14 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.getElementById('hamburger');
+    const navMenu  = document.getElementById('nav-menu');
+
+    hamburger.addEventListener('click', () => {
+        navMenu.classList.toggle('active');
+        // animación de las líneas (opcional)
+        hamburger.classList.toggle('open');
+    });
+});
